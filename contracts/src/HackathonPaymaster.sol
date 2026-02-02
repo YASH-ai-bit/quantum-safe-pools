550132ce55cec8fbaa7d9970fd20d031bbd7f348
Samarth <samarth208p@gmail.com>
2026-02-02 19:53:39 +0530

    fix: use solc 0.8.28 for account-abstraction compatibility

--- contracts/src/HackathonPaymaster.sol
+++ contracts/src/HackathonPaymaster.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.28;
 
 import "@account-abstraction/contracts/core/BasePaymaster.sol";
 import "@account-abstraction/contracts/interfaces/PackedUserOperation.sol";
